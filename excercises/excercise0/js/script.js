6281500dc70883468f0cd6cbef7c148f7e46b2f9
Tran <huyentran317@gmail.com>
2018-09-13 16:49:34 -0400

    Added excercise0

---
+++ excercises/excercise0/js/script.js
@@ -0,0 +1,32 @@
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+// setup()
+//
+// Description of setup
+var codeImage;
+
+function preload() {
+codeImage = LoadImage("assets/images/exampleImage.png")
+
+
+
+
+}
+
+
+// draw()
+//
+// Description of draw()
+
+function draw() {
+
+
+}
