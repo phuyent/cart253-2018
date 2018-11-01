f14bfb57be18206e3364321015b290fb56b1c9ff
HUYEN TRAN PHAM <p_huyent@cda070ev5815x.local>
2018-11-01 16:23:02 -0400

    Ex6-part5-error4
    The problem: A redundant '(' in 'function(paddle)' and misspell 'prototype'
    Fix: Erase the extra '(' and correct the typo in 'prototype'

--- excercises/broken-game-basic-oop-pong/js/Ball.js
+++ excercises/broken-game-basic-oop-pong/js/Ball.js
@@ -63,7 +63,8 @@ Ball.prototype.display = function () {
 //
 // Check if this ball overlaps the paddle passed as an argument
 // and if so reverse x velocity to bounce
-Ball.prototyp.handleCollision = function((paddle) {
+/////FIXED ERROR 4 (ACCORDING TO CONSOLE): A redundant '(' & misspell 'prototype'/////
+Ball.prototype.handleCollision = function(paddle) {
   // Check if the ball overlaps the paddle on x axis
   if (this.x + this.size > paddle.x && this.x < paddle.x + paddle.w) {
     // Check if the ball overlaps the paddle on y axis
