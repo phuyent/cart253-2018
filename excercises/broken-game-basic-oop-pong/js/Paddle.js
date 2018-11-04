b143fc7cc4e9ecb17c198f5771cb9edb0d33c4a4
phuyent <huyentran317@gmail.com>
2018-11-04 05:09:58 -0500

    Ex6-part20-error19
    The problem: Misspelling 'constrain' and 'height'
    Fix: Correct the typo

--- excercises/broken-game-basic-oop-pong/js/Paddle.js
+++ excercises/broken-game-basic-oop-pong/js/Paddle.js
@@ -44,7 +44,9 @@ Paddle.prototype.handleInput = function() {
 // Constrain the resulting position to be within the canvas
 Paddle.prototype.update = function() {
   this.y += this.vy;
-  this.y = constraint(this.y,0,hight-this.h);
+/////FIXED ERROR 19: Misspell 'constrain' & 'height'/////
+  this.y = constrain(this.y,0,height-this.h);
+/////END FIXED ERROR 19/////
 }
 
 // display()
