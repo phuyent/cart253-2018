f9d81db8cee9bb2746c1d3cbce8e155ba0dd548d
phuyent <huyentran317@gmail.com>
2018-11-03 23:52:39 -0400

    Ex6-part11-error10
    The problem: Wrong 'createCanvas()' syntax
    Fix: Correct typo

--- excercises/broken-game-basic-oop-pong/js/script.js
+++ excercises/broken-game-basic-oop-pong/js/script.js
@@ -18,7 +18,9 @@ var rightPaddle;
 //
 // Creates the ball and paddles
 function setup() {
-  crateCanvas(640,480);
+/////FIXED ERROR 10 (ACCORDING TO CONSOLE): Wrong creatCanvas() syntax/////
+  createCanvas(640,480);
+/////END FIXED ERROR 10/////
   noStroke();
   // Create a ball
   ball = new Ball(width/2,height/2,50,50,10,50);
