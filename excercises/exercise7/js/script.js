41c29e0def5c6cec401fd0a775befe3d9b4dabcc
phuyent <43248395+phuyent@users.noreply.github.com>
2018-11-15 16:14:33 -0500

    Ex7-part1
    Started exercise 7 from scratch with explanations on top of 'script.js'

---
+++ excercises/exercise7/js/script.js
@@ -0,0 +1,39 @@
+//Exercise 7
+//Prototype Project 3
+//
+//Huyen Tran Pham
+//
+//This is a proposal design for the final project in CART 253
+//The final project will use this exercise features. 
+//I envisage project 3 to be a game with 3 levels
+//Each level will have a different theme but the rule does not change much
+//The game for me is interactive, creativve and it also shows many ideas in each level
+
+
+
+
+// preload()
+//
+// Description of preload
+
+function preload() {
+
+}
+
+
+// setup()
+//
+// Description of setup
+
+function setup() {
+
+}
+
+
+// draw()
+//
+// Description of draw()
+
+function draw() {
+
+}
