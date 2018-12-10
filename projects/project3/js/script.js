6ab11acc788f6b284e662b59c0a1c4e5a4c412f7
phuyent <huyentran317@gmail.com>
2018-12-10 15:09:16 -0500

    P3-Part1
    Started project 3

---
+++ projects/project3/js/script.js
@@ -0,0 +1,34 @@
+
+//PROJECT 3: THE HOLY GRAND FINALE
+//Huyen Tran Pham
+
+//This is a 3-level game with a scoring system
+//The player will get bigger when it overlaps (eats) other objects on the canvas.
+//There will be threats needed to avoid.
+//The player can only pass if he/she reaches the level goal
+
+// preload()
+//
+// Description of preload
+
+function preload() {
+
+}
+
+
+// setup()
+//
+// Description of setup
+
+function setup() {
+
+}
+
+
+// draw()
+//
+// Description of draw()
+
+function draw() {
+
+}
