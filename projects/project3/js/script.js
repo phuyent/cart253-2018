92f4942aa193db5ea5ed05c6568b52b71ffbb0df
phuyent <huyentran317@gmail.com>
2018-12-12 08:04:52 -0500

    P3-Part7
    Add all three levels

--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -197,6 +197,122 @@ function levelOne() {
   }
   ////***DONE WITH TIMER***////
 }
+function levelThree() {
+  ////***Level 3 theme will be in space, searching for the yellow gemstones***////
+  //New background to pair with the theme
+  push();
+  imageMode(CORNERS);
+  image(background3,0,0,windowWidth,windowHeight);
+  //Display the goal at the top right of the canvas
+  text ('Eat 15 gemstone to pass',600,50);
+  pop();
+
+  ////Handles input, updates all the elements, checks for collisions
+  //and displays everything.
+
+  //Monster functions
+  monster.handleInput();
+  monster.display();
+  monster.update();
+  monster.displayScoreLevelThree();
+  monster.handleCollision(rocks);
+  monster.handleCollision(gemstones);
+  if (monster.isOffScreen()) {
+    monster.reset();
+  }
+
+  //Rock functions
+  for(i=0; i<10;i++){
+    rocks[i].update();
+    rocks[i].display();
+
+  }
+  //Gemstone functions
+  for(i=0; i<10;i++){
+    gemstones[i].update();
+    gemstones[i].displayLevelThree();
+  }
+
+  ////***SET UP THE TIMER***////
+  //Source: https://editor.p5js.org/marynotari/sketches/S1T2ZTMp-
+  //Styling the font
+  textStyle('Helvetica');
+  textAlign(CENTER);
+  textSize(30);
+  //Display the countdown at the top left of the canvas
+  text('Time:'+timer,100,50);
+  // frameCount: this keeps track of the number of times the program has gone throught the code, 60 = 1 second
+  // %: this is the Modulo operator, it divides numbers and evaluates to the remainder: 17 % 5 evaluates to 2 remainder
+  // This can be used to determine if the number on the left is divisible by the number on the right
+  if (frameCount % 60 == 0 && timer > 0) {
+    //If the frameCount is divisible by 60, then a second has passed.
+    //It will stop at 0
+    timer --;
+  }
+  if (timer == 0) {
+    text("TIME'S UP!", width/2, height/3);
+    text("Press 'Enter' to start again", width/2,height/5);
+  }
+  ////***DONE WITH TIMER***////
+}
+function levelOne() {
+  ////***Level 1 theme will be on land, searching for the green and red gemstones***////
+  //New background to pair with the theme
+  push();
+  imageMode(CORNERS);
+  image(background1,0,0,windowWidth,windowHeight);
+  //Display the goal at the top right of the canvas
+  text ('Eat 5 gemstone to pass',600,50);
+  pop();
+
+  ////Handles input, updates all the elements, checks for collisions
+  //and displays everything.
+
+  //Monster functions
+  monster.handleInput();
+  monster.display();
+  monster.update();
+  monster.displayScoreLevelOne();
+  monster.handleCollision(rocks);
+  monster.handleCollision(gemstones);
+  if (monster.isOffScreen()) {
+    monster.reset();
+  }
+
+  //Rock functions
+  for(i=0; i<10;i++){
+    rocks[i].update();
+    rocks[i].display();
+
+  }
+  //Gemstone functions
+  for(i=0; i<10;i++){
+    gemstones[i].update();
+    gemstones[i].displayLevelOne();
+  }
+
+  ////***SET UP THE TIMER***////
+  //Source: https://editor.p5js.org/marynotari/sketches/S1T2ZTMp-
+  //Styling the font
+  textStyle('Helvetica');
+  textAlign(CENTER);
+  textSize(30);
+  //Display the countdown at the top left of the canvas
+  text('Time:'+timer,100,50);
+  // frameCount: this keeps track of the number of times the program has gone throught the code, 60 = 1 second
+  // %: this is the Modulo operator, it divides numbers and evaluates to the remainder: 17 % 5 evaluates to 2 remainder
+  // This can be used to determine if the number on the left is divisible by the number on the right
+  if (frameCount % 60 == 0 && timer > 0) {
+    //If the frameCount is divisible by 60, then a second has passed.
+    //It will stop at 0
+    timer --;
+  }
+  if (timer == 0) {
+    text("TIME'S UP!", width/2, height/3);
+    text("Press 'Enter' to start again", width/2,height/5);
+  }
+  ////***DONE WITH TIMER***////
+}
 function gameEnd(){
   imageMode(CORNER);
   image(background4,0,0,windowWidth,windowHeight);
