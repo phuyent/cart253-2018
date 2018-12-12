1ac1497b8d1f29428afa2c2e06dae19496b1f8d9
phuyent <huyentran317@gmail.com>
2018-12-11 20:42:22 -0500

    P3-Part3
    Added visuals, set properties,functions for level 1

--- projects/project3/js/monster.js
+++ projects/project3/js/monster.js
@@ -6,11 +6,112 @@
 //Monster constructor
 //
 //Sets the properties
-function Monster(x,y,vx,vy,size,speed){
+function Monster(x,y,w,h,size,speed,score,scorePositionX,scorePositionY,upKey,downKey,leftKey,rightKey,monsterImage){
   this.x = x;
   this.y = y;
-  this.vx = vx;
-  this.vy = vy;
+  this.w = w;
+  this.h = h;
+  this.vx = 0;
+  this.vy = 0;
   this.size = size;
   this.speed = speed;
+  this.score = score;
+  this.scorePositionX = scorePositionX;
+  this.scorePositionY = scorePositionY;
+  this.upKey = upKey;
+  this.downKey = downKey;
+  this.leftKey = leftKey;
+  this.rightKey = rightKey;
+  this.monsterImage = monsterImage;
+}
+
+//handleInput()
+//
+// Check if the up,down,left,right keys are pressed and update velocity
+Monster.prototype.handleInput = function() {
+  if (keyIsDown(this.upKey)) {
+    this.vy = -this.speed;
+  }
+  else if (keyIsDown(this.downKey)) {
+    this.vy = this.speed;
+  }
+  else if (keyIsDown(this.leftKey)) {
+    this.vx = -this.speed;
+  }
+  else if (keyIsDown(this.rightKey)){
+    this.vx = this.speed;
+  }
+  else {
+    this.vx = 0;
+    this.vy = 0;
+  }
+}
+
+// update()
+// Update x, y position based on velocity
+// Constrain the resulting position to be within the canvas
+Monster.prototype.update = function() {
+  this.x += this.vx;
+  this.y += this.vy;
+  this.x = constrain(this.x,0,width-this.w);
+  this.y = constrain(this.y,0,height-this.h);
+}
+//isOffScreen()
+//
+//If the player is running out of the screen
+Monster.prototype.isOffScreen = function(){
+  // Check for going off screen and reset if so
+  if (this.x + this.size < 0 || this.x > width) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+// handleCollision(gemstone)
+//
+// Check if this monster overlaps the gemstone passed as an argument
+// and if so plus one point and enlarge
+Monster.prototype.handleCollision = function(gemstone) {
+  // Check if the monster overlaps the gemstone on x axis
+  if (this.x + this.size >gemstone.x && this.x < gemstone.x + gemstone.w) {
+    // Check if the monster overlaps the gemstone on y axis
+    if (this.y + this.size > gemstone.y && this.y < gemstone.y + gemstone.h) {
+      // If so, expand the monster size
+      this.size +=5;
+      //Add point to the monster
+      this.score +=1;
+    }
+  }
+}
+// handleCollision(rock)
+//
+// Check if this monster overlaps the gemstone passed as an argument
+// and if so minus one and shrink the monster
+Monster.prototype.handleCollision = function(rock) {
+  // Check if the monster overlaps the rock on x axis
+  if (this.x + this.size > rock.x && this.x < rock.x + rock.w) {
+    // Check if the monster overlaps the rock on y axis
+    if (this.y + this.size > rock.y && this.y < rock.y + rock.h) {
+      // If so, shrink the monster size
+      this.size -=5;
+      //Minus point to the monster
+      this.score -=1;
+    }
+  }
+}
+
+// display()
+//
+//Display the monster image
+Monster.prototype.display = function() {
+  noStroke();
+  image(this.monsterImage,this.x,this.y,this.w,this.h);
+}
+
+//Display the score under the timer
+Monster.prototype.displayScore = function() {
+  text('Score: ' + this.score,this.scorePositionX,this.scorePositionY);
+  textSize(20);
+  fill(255);
 }
--- projects/project3/js/script.js
+++ projects/project3/js/script.js
@@ -18,22 +18,30 @@ var state = 0;
 var monster;
 var monsterImage;
 //Array containing mutiple colored gemstones
-var gemstone =[
-  green,
-  red,
-  blue,
-  white,
-  yellow,
-  pink
+var gemstone = [
+  new Gemstone(10,10,2,2,10,2),
+  new Gemstone(20,10,2,2,10,2),
+  new Gemstone(30,10,2,2,10,2),
+  new Gemstone(40,10,2,2,10,2),
+  new Gemstone(50,10,2,2,10,2),
+  new Gemstone(60,10,2,2,10,2),
+  new Gemstone(70,10,2,2,10,2),
+  new Gemstone(80,10,2,2,10,2),
+  new Gemstone(90,10,2,2,10,2),
+  new Gemstone(100,10,2,2,10,2),
+  new Gemstone(110,10,2,2,10,2),
+  new Gemstone(120,10,2,2,10,2),
+  new Gemstone(130,10,2,2,10,2),
+  new Gemstone(140,10,2,2,10,2),
+  new Gemstone(150,10,2,2,10,2)
 ];
 //Start rock with an empty array to later define its position
 var rock =[];
-//Three backgrounds for 3 levels
-var backgroundImage = [
-  level1,
-  level2,
-  level3
-];
+//Three backgrounds for 3 levels and the title background
+var background0;
+var background1;
+var background2;
+var bachground3;
 //A specific opening song that pairs ell with my eating theme
 var openMusic;
 
@@ -41,8 +49,13 @@ var openMusic;
 //
 // Load the images and music before the program runs
 function preload() {
-//The opening song
- openMusic = new Audio('assets/sounds/nom-nom-song.mp3');
+  //The opening song
+  openMusic = new Audio('assets/sounds/nom-nom-song.mp3');
+  background0 = loadImage('assets/images/bg0.png');
+  background1 = loadImage('assets/images/bg1.jpg');
+  background2 = loadImage('assets/images/bg2.jpg');
+  background3 = loadImage('assets/images/bg3.jpg');
+  monsterImage = loadImage('assets/images/monster.png');
 }
 
 
@@ -51,7 +64,12 @@ function preload() {
 //Create the monster and objects for each level
 function setup() {
   createCanvas(800,600);
-
+  //Create the gemstone
+  gemstone = new Gemstone(150,150,10,5);
+  //Create the rock
+  rock = new Rock(100,100,5,5,10,5);
+  //Create the monster
+  monster = new Monster(width/2,height-100,80,70,70,10,0,100,90,38,40,37,39,monsterImage);
 }
 
 
@@ -61,22 +79,21 @@ function setup() {
 //and displays everything.
 
 function draw() {
-  background(220,0,220);
   switch(state) {
     case 0:
     gameTitle();
     break;
 
     case 1:
-    level1();
+    levelOne();
     break;
 
     case 2:
-    level2();
+    levelTwo();
     break;
 
     case 3:
-    level3();
+    levelThree();
     break;
 
     case 4:
@@ -85,20 +102,51 @@ function draw() {
   }
 }
 function gameTitle(){
-  openMusic.play();
+  image(background0,0,0,windowWidth,windowHeight);
   fill(255);
   text('THE GEM EATER', width/2, height/2);
-  text('Press spacebar to start eating!!!',width/2, height*0.7)
+  text('Join in to be the master of power',width/2, height*0.6);
+  text('Press spacebar to start eating!!!',width/2, height*0.7);
   textFont('Georgia',30);
   textStyle(BOLD);
   textAlign(CENTER);
 }
-function level1() {
+function levelOne() {
   ////***Level 1 theme will be on land, searching for the green and red gemstones***////
   //New background to pair with the theme
-  backgroundImage[level1] = image()
+  push();
+  imageMode(CORNERS);
+  image(background1,0,0,windowWidth,windowHeight);
   //Display the goal at the top right of the canvas
   text ('Eat 5 gemstone to pass',600,50);
+  pop();
+  ////Handles input, updates all the elements, checks for collisions
+  //and displays everything.
+  monster.handleInput();
+
+  monster.update();
+  gemstone.update();
+  rock.update();
+
+  if (monster.isOffScreen()) {
+    monster.reset();
+  }
+
+  monster.handleCollision(gemstone);
+  monster.handleCollision(rock);
+
+  monster.display();
+  gemstone.display();
+  rock.display();
+
+  monster.displayScore();
+
+  if (rock.isOffScreen()) {
+    rock.reset();
+  }
+
+  rock.handleCollision(monster);
+  gemstone.handleCollision(monster);
 
   ////***SET UP THE TIMER***////
   //Styling the font
@@ -117,18 +165,17 @@ function level1() {
     timer --;
   }
   if (timer == 0) {
-    text("GAME OVER", width/2, height*0.7);
+    text("TIME'S UP!", width/2, height/2);
   }
   ////***DONE WITH TIMER***////
-
 }
 function keyPressed() {
-//Press spacebar to start the game
+  //Press spacebar to start the game
   if (keyCode === 32) {
     state = 1;
   }
-//Press 'enter' key to play again
-if (keyCode === 13){
-  state = 0;
-}
+  //Press 'enter' key to play again
+  if (keyCode === 13){
+    state = 0;
+  }
 }
